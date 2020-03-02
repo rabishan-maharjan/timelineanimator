5552ca3f49d44888a943f3dae9a6b5acd7eee349
Rabishan <emialRabishan@gmail.com>
2020-03-02 18:26:44 +0545

    added rotation

--- Runtime/PathJump.cs
+++ Runtime/PathJump.cs
@@ -5,7 +5,7 @@ namespace Arcube.TimelineAnimator {
     public class PathJump : MonoBehaviour, IAnimatable {
         [ReorderableList] [SerializeField] Transform []points;
         [SerializeField] float jumpMaxPosition = 0;
-        [SerializeField] System.Type type;
+
         public void Animate(float progress, int from, int to) {
             Vector3 pos = Vector3.Lerp(points[from].position, points[to].position, progress);
             pos.y += Mathf.Sin(progress * Mathf.PI) * jumpMaxPosition;
--- Runtime/Rotate.cs
+++ Runtime/Rotate.cs
@@ -3,15 +3,17 @@ using NaughtyAttributes;
 using System.Collections.Generic;
 
 namespace Arcube.TimelineAnimator {
-    public class Scale : MonoBehaviour, IAnimatable {
+    public class Rotate : MonoBehaviour, IAnimatable {
         [ReorderableList] [SerializeField] List<Vector3> points = new List<Vector3>();
 
         [Button]
-        public void AddPoint() => points.Add(transform.localScale);
+        public void AddPoint() => points.Add(transform.localRotation.eulerAngles);
 
         public void Animate(float progress, int from, int to) {
             Vector3 value = Vector3.Lerp(points[from], points[to], progress);
-            transform.localScale = value;
+            Quaternion rot = transform.localRotation;
+            rot.eulerAngles = value;
+            transform.localRotation = rot;
         }
     }
 }
