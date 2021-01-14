6e2c412f6cbb99f8cfbaa505356fdc547387f95b
rabishan <maharjan.rabishan@gmail.com>
2021-01-14 12:21:56 +0545

    added curve move

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,5 @@ Version 1.0.2
 Added Setter buttons in clip inspector
 Version 1.0.5
 Added support for Children Animation
+Version 1.0.6
+Added support for Moving along curve
---
+++ Runtime/CurveMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Arcube.TimelineAnimator {
+    [AddComponentMenu("TimelineEditor/CurveMove")]
+    public class CurveMove : MonoBehaviour, IAnimatable {
+        [SerializeField] float []points;
+        [SerializeField] float height = 5;
+        public void Animate(float progress, int from, int to) {
+            
+        }
+
+        public void AnimateChildren(float progress, AnimationCurve curve, int from, int to) {
+            float curveProgress = curve.Evaluate(progress);
+                
+            Vector3 value = transform.position;
+            value.x = Mathf.Lerp(points[from], points[to], progress);
+            value.y = curveProgress * height;
+
+            transform.SetPositionLocal(value);
+        }
+    }
+}
