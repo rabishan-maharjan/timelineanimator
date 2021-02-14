45a8175828270241f2af08da45c61c08cb5d0fef
rabishan <maharjan.rabishan@gmail.com>
2021-02-14 13:43:32 +0545

    added recttransform on curvemove

--- Runtime/CurveMove.cs
+++ Runtime/CurveMove.cs
@@ -3,7 +3,7 @@
 namespace Arcube.TimelineAnimator {
     [AddComponentMenu("TimelineEditor/CurveMove")]
     public class CurveMove : MonoBehaviour, IAnimatable {
-        [SerializeField] float []points;
+        [SerializeField] float []points = null;
         [SerializeField] float height = 5;
         public void Animate(float progress, int from, int to) {
             
@@ -11,12 +11,20 @@ namespace Arcube.TimelineAnimator {
 
         public void AnimateChildren(float progress, AnimationCurve curve, int from, int to, bool ascendingOrder = true) {
             float curveProgress = curve.Evaluate(progress);
-                
-            Vector3 value = transform.position;
-            value.x = Mathf.Lerp(points[from], points[to], progress);
-            value.y = curveProgress * height;
 
-            transform.SetPositionLocal(value);
+            if (TryGetComponent(out RectTransform rt)) {
+                Vector2 value = rt.anchoredPosition;
+                value.x = Mathf.Lerp(points[from], points[to], progress);
+                value.y = curveProgress * height;
+
+                rt.anchoredPosition = value;
+            } else {
+                Vector3 value = transform.position;
+                value.x = Mathf.Lerp(points[from], points[to], progress);
+                value.y = curveProgress * height;
+
+                transform.SetPositionLocal(value);
+            }
         }
     }
 }
